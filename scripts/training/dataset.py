cddced1d54adeb69a3b71408a4160be3ac1939a1
viktprog <viktprog@gmail.com>
2021-02-12 19:58:13 +0900

    fix: update dataset module

--- scripts/training/dataset.py
+++ scripts/training/dataset.py
@@ -2,6 +2,7 @@
 
 """Dataset routines."""
 
+import random
 import tensorflow as tf
 
 
@@ -29,15 +30,15 @@ def get_dataset(path, batch_size=64, num_test_set=100, crop_size=32):
     """
     def parse_example(example):
         return tf.io.parse_single_example(example, {
-            'input': tf.io.FixedLenFeature([10], tf.string),
-            'target': tf.io.FixedLenFeature([10], tf.string),
+            "input": tf.io.FixedLenFeature([10], tf.string),
+            "target": tf.io.FixedLenFeature([10], tf.string),
         })
 
     def parse_img(val):
         return {
-            "input": tf.map_fn(tf.io.decode_png, val["input"],
+            "input": tf.map_fn(tf.io.decode_image, val["input"],
                                fn_output_signature=tf.uint8),
-            "target": tf.map_fn(tf.io.decode_png, val["target"],
+            "target": tf.map_fn(tf.io.decode_image, val["target"],
                                 fn_output_signature=tf.uint8),
         }
 
@@ -64,7 +65,14 @@ def get_dataset(path, batch_size=64, num_test_set=100, crop_size=32):
                                  [10, crop_size*2, crop_size*2, 3]) / 255.0,
         }
 
+    def filter_flat(val):
+        d = val["input"]
+        d = tf.math.reduce_std(d, axis=0)
+        d = tf.math.reduce_sum(d) / crop_size / crop_size
+        return d > 0.01
+
     records = tf.io.gfile.glob(path)
+    random.shuffle(records)
     ds = tf.data.TFRecordDataset(
         records,
         num_parallel_reads=tf.data.experimental.AUTOTUNE
@@ -85,9 +93,11 @@ def get_dataset(path, batch_size=64, num_test_set=100, crop_size=32):
     ds = ds.repeat()
     ds = ds.map(random_crop)
     ds = ds.map(normalize)
+    ds = ds.filter(filter_flat)
     ds = ds.batch(batch_size, drop_remainder=True)
     val_ds = val_ds.repeat(10)
     val_ds = val_ds.map(random_crop)
     val_ds = val_ds.map(normalize)
+    val_ds = val_ds.filter(filter_flat)
     val_ds = val_ds.batch(batch_size, drop_remainder=True)
     return ds, val_ds
