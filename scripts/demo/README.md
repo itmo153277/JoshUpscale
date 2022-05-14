5f3d0e5d946fbfbe84f9087774f7606da2520584
Ivanov Viktor <viktprog@gmail.com>
2022-05-14 18:35:29 +0900

    Remove old code

--- scripts/demo/README.md
+++
@@ -1,9 +0,0 @@
-# Demo AviSynth Scripts
-
-## demo.avs
-
-Creates comparison video between original video and upscaled one.
-
-## transcode.avs
-
-Script for transcoding original video with upscaling applied.
--- src/gui/sdl_wrappers.cc
+++
@@ -1,9 +0,0 @@
-// Copyright 2021 Ivanov Viktor
-
-#include "sdl_wrappers.h"
-
-void sdl::init() {
-	sdl::callOrThrow(
-	    ::SDL_Init, SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_EVENTS);
-	std::atexit(::SDL_Quit);
-}
