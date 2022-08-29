77b467465c96559d7a80b742bce6f0d12a1be7e4
Ivanov Viktor <viktprog@gmail.com>
2022-08-29 21:46:43 +0900

    Add scene detection for moving average

--- scripts/inference/onnx/frame_moving_avg.py
+++ scripts/inference/onnx/frame_moving_avg.py
@@ -35,6 +35,11 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("-s", "--strength",
                         help="Filter strength (default: %(default).3f)",
                         type=float,
+                        default=0.25)
+    parser.add_argument("-t", "--threshold",
+                        help=("Scene detection threshold " +
+                              "(default: %(default).3f)"),
+                        type=float,
                         default=0.1)
     return parser.parse_args()
 
@@ -49,6 +54,7 @@ def main(
     output_path: str,
     num_checks: int,
     strength: float,
+    threshold: float,
 ) -> int:
     """
     Run CLI.
@@ -63,6 +69,8 @@ def main(
         Number of simplifier checks
     strength: float
         Filter strength
+    threshold: float
+        Scene detection threshold
 
     Returns
     -------
@@ -76,14 +84,42 @@ def main(
     target_node = graph.find_node_by_name(TARGET_NODE)
     input_tensor = input_node.input[0]
     output_tensor = target_node.output[0]
-    mask = np.array(strength, dtype=np.float32).reshape((1, 1, 1, 1))
 
     graph.remove_node(target_node)
     target_node.output[0] = "output_pre_mask"
     graph.insert_node(target_node)
 
-    graph.create_constant("output_mask", mask)
-    graph.create_constant("output_mask_2", 1 - mask)
+    graph.create_node(
+        "output_diff", "Sub", ["output_pre_mask", input_tensor]
+    )
+    graph.create_node(
+        "output_diff_abs", "Abs", ["output_diff"]
+    )
+    graph.create_node(
+        "output_diff_mean", "ReduceMean", ["output_diff_abs"]
+    )
+    graph.create_constant("scene_threshold", np.float32(threshold))
+    graph.create_node(
+        "scene_th_diff", "Sub", ["output_diff_mean", "scene_threshold"]
+    )
+    graph.create_node(
+        "scene_cond", "Sign", ["scene_th_diff"]
+    )
+    graph.create_constant("output_mask_const_1", np.float32(strength / 2))
+    graph.create_constant("output_mask_const_2", np.float32(-strength / 2))
+    graph.create_constant("output_mask_const_3", np.float32(1 - strength / 2))
+    graph.create_node(
+        "output_mask_a", "Mul", ["scene_cond", "output_mask_const_2"]
+    )
+    graph.create_node(
+        "output_mask", "Add", ["output_mask_a", "output_mask_const_1"]
+    )
+    graph.create_node(
+        "output_mask_b", "Mul", ["scene_cond", "output_mask_const_1"]
+    )
+    graph.create_node(
+        "output_mask_2", "Add", ["output_mask_b", "output_mask_const_3"]
+    )
     graph.create_node(
         "output_masked", "Mul", [input_tensor, "output_mask"],
     )
