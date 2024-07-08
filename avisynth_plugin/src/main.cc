5e12a8e0a5eb810e752c55fe6b10d44f08800835
Viktor Ivanov <viktprog@gmail.com>
2024-07-08 22:26:30 +0900

    Implement proper MT and caching from avisynth

--- avisynth_plugin/src/main.cc
+++ avisynth_plugin/src/main.cc
@@ -6,7 +6,9 @@
 #include <avisynth.h>
 
 #include <cstddef>
+#include <iostream>
 #include <memory>
+#include <vector>
 
 namespace JoshUpscale {
 
@@ -14,15 +16,23 @@ namespace avisynth {
 
 namespace {
 
+constexpr int MAX_BACKTRACK_SIZE = 32;
+constexpr std::size_t CACHE_SIZE = 64;
+
 class JoshUpscaleFilter : public GenericVideoFilter {
 public:
 	JoshUpscaleFilter(PClip _child, IScriptEnvironment *env,
 	    const char *modelPath, core::Quantization quantization);
 	~JoshUpscaleFilter();
-	PVideoFrame __stdcall GetFrame(int n, IScriptEnvironment *env);
+	PVideoFrame __stdcall GetFrame(int n, IScriptEnvironment *env) override;
+	int __stdcall SetCacheHints(int cacheHints, int frameRange) override;
 
 private:
 	std::unique_ptr<core::Runtime> m_Runtime;
+	int m_NextFrame = 0;
+	bool m_StopBacktrackWarning = false;
+	std::vector<PVideoFrame> m_Cache;
+	std::size_t m_CacheShift = 0;
 };
 
 JoshUpscaleFilter::JoshUpscaleFilter(PClip _child,  // NOLINT
@@ -35,10 +45,11 @@ JoshUpscaleFilter::JoshUpscaleFilter(PClip _child,  // NOLINT
 	if (vi.width != core::INPUT_WIDTH || vi.height != core::INPUT_HEIGHT) {
 		env->ThrowError("JoshUpscale: unsupported video size");
 	}
+	env->CheckVersion(8);
 	vi.width = core::OUTPUT_WIDTH;
 	vi.height = core::OUTPUT_HEIGHT;
-	child->SetCacheHints(CACHE_ACCESS_SEQ1, 0);
 	m_Runtime.reset(core::createRuntime(0, modelPath, quantization));
+	m_Cache.reserve(MAX_BACKTRACK_SIZE);
 }
 
 JoshUpscaleFilter::~JoshUpscaleFilter() {
@@ -46,25 +57,88 @@ JoshUpscaleFilter::~JoshUpscaleFilter() {
 
 PVideoFrame __stdcall JoshUpscaleFilter::GetFrame(
     int n, IScriptEnvironment *env) {
+	if (n < m_NextFrame) {
+		std::size_t offset = static_cast<std::size_t>(m_NextFrame - n);
+		if (offset <= m_Cache.size()) {
+			return m_Cache[(m_Cache.size() - offset + m_CacheShift) %
+			               CACHE_SIZE];
+		}
+		std::clog << "[JoshUpscaleAvisynth] WARNING: Resetting stream"
+		          << std::endl;
+		// Redo all frames
+		m_NextFrame = 0;
+		m_Cache.clear();
+		m_CacheShift = 0;
+	}
+	if (n > m_NextFrame) {
+		if (m_NextFrame + MAX_BACKTRACK_SIZE < n) {
+			m_NextFrame = n - MAX_BACKTRACK_SIZE;
+		}
+		if (!m_StopBacktrackWarning) {
+			std::clog
+			    << "[JoshUpscaleAvisynth] WARNING: Backtracking stream from "
+			    << m_NextFrame << " to " << n << std::endl;
+			m_StopBacktrackWarning = true;
+		}
+		// Backtrack to our frame
+		GetFrame(n - 1, env);
+	}
+	if (n != m_NextFrame) {
+		env->ThrowError(
+		    "JoshUpscale: expected frame %d, got %d", m_NextFrame, n);
+	}
+	m_StopBacktrackWarning = false;
 	PVideoFrame src = child->GetFrame(n, env);
-	PVideoFrame dst = env->NewVideoFrame(vi);
-	env->MakeWritable(&dst);
-	core::Image inputImage;
-	inputImage.width = 480;
-	inputImage.height = 270;
-	inputImage.stride = -src->GetPitch();
-	inputImage.ptr = const_cast<BYTE *>(
-	    src->GetReadPtr() +
-	    (static_cast<std::ptrdiff_t>(src->GetHeight()) - 1) * src->GetPitch());
-	core::Image outputImage;
-	outputImage.width = 1920;
-	outputImage.height = 1080;
-	outputImage.stride = -dst->GetPitch();
-	outputImage.ptr =
-	    dst->GetWritePtr() +
-	    (static_cast<std::ptrdiff_t>(dst->GetHeight()) - 1) * dst->GetPitch();
+	PVideoFrame dst = env->NewVideoFrameP(vi, &src);
+	core::Image inputImage = {
+	    .ptr = const_cast<BYTE *>(
+	        src->GetReadPtr() +
+	        (static_cast<std::ptrdiff_t>(core::INPUT_HEIGHT) - 1) *
+	            src->GetPitch()),
+	    .stride = -src->GetPitch(),
+	    .width = core::INPUT_WIDTH,
+	    .height = core::INPUT_HEIGHT,
+	};
+	core::Image outputImage = {
+	    .ptr = dst->GetWritePtr() +
+	           (static_cast<std::ptrdiff_t>(core::OUTPUT_HEIGHT) - 1) *
+	               dst->GetPitch(),
+	    .stride = -dst->GetPitch(),
+	    .width = core::OUTPUT_WIDTH,
+	    .height = core::OUTPUT_HEIGHT,
+	};
 	m_Runtime->processImage(inputImage, outputImage);
+	m_NextFrame = n + 1;
+	if (m_Cache.size() == CACHE_SIZE) {
+		m_Cache[m_CacheShift] = dst;
+		++m_CacheShift;
+		m_CacheShift %= CACHE_SIZE;
+	} else {
+		m_Cache.push_back(dst);
+	}
 	return dst;
+}
+
+int __stdcall JoshUpscaleFilter::SetCacheHints(
+    int cacheHints, [[maybe_unused]] int frameRange) {
+	switch (cacheHints) {
+	// Deprecated hints
+	case CACHE_GETCHILD_COST:
+		return CACHE_COST_HI;
+	case CACHE_GETCHILD_THREAD_MODE:
+		return CACHE_THREAD_CLASS;
+	case CACHE_GETCHILD_ACCESS_COST:
+		return CACHE_ACCESS_SEQ1;
+	// We run on CUDA, but accept and output for CPU
+	case CACHE_GET_DEV_TYPE:
+	case CACHE_GET_CHILD_DEV_TYPE:
+		return DEV_TYPE_CPU;
+	// Can't run in parallel
+	case CACHE_GET_MTMODE:
+		return MT_SERIALIZED;
+	default:
+		return 0;
+	}
 }
 
 AVSValue __cdecl CreateFilter(AVSValue args,  // NOLINT
